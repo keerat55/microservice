05d06b9ae3d29068f9a7b9b462894f98284c1a49
Keerat Rashid <77684491+keerat55@users.noreply.github.com>
2023-05-26 13:20:58 +0500

    Add files via upload

---
+++ microservices/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using microservices.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace microservices.Repository
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        IProductRepository GetProductByID(int ProductId);
+        void InsertProduct(Product Product);
+        void DeleteProduct(int ProductId);
+        void UpdateProduct(Product Product);
+        void Save();
+
+    }
+}
